113311975d92c151a81827b9bbf58f6452c26a5d
penguyen <peternguyen008@gmail.com>
2018-06-27 17:21:22 -0400

    npm install typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Hi, my name is Peter!',
+    test: 'Pita',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: [
+  	'gatsby-plugin-react-helmet',
+  	{
+  		resolve: `gatsby-plugin-typography`,
+  		options: {
+  			pathToConfigModule: `src/utils/typography.js`,
+  		},
+  	},
+  ],
+};
