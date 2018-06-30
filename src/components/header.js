83d67500ff86bcada91c0701ef96445ff4ab2045
penguyen <peternguyen008@gmail.com>
2018-06-30 18:32:47 -0400

    saving - responsive via css positions

--- src/components/header.js
+++ src/components/header.js
@@ -7,15 +7,17 @@ import '../stylesheets/sass/partials/header.scss'
 
 export default () => (
   <div id='header'>
-    <img className='header-pic' src={me} />
-    <h1>Peter Nguyen</h1>
-    <h5>New York | Boston</h5>
-    <ul className='header-list'>
-      <li>Software Developer</li>
-      <li>Athlete</li>
-      <li>Foodie</li>
-      <li>Sneakerhead</li>
-      <li>Coffee Lover</li>
-    </ul>
+    <div id='header-contents'>
+      <img className='header-pic' src={me} />
+      <h1>Peter Nguyen</h1>
+      <h5>New York | Boston</h5>
+      <ul className='header-list'>
+        <li>Software Developer</li>
+        <li>Athlete</li>
+        <li>Foodie</li>
+        <li>Sneakerhead</li>
+        <li>Coffee Lover</li>
+      </ul>
+    </div>
   </div>
 );
